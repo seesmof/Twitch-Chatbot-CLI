b2de0388febb1635706387ba1b8286df0d4a5d22
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-05 19:43:52 +0300

    fix: remove even more unnecessary imports

--- mfs.py
+++ mfs.py
@@ -2,9 +2,8 @@ import openai
 import os
 import asyncio
 import gpt4free
-from gpt4free import Provider, quora, forefront
+from gpt4free import Provider
 from deep_translator import GoogleTranslator
-from langdetect import detect
 from vars import *
 from datetime import datetime
 
