906a605c058479ebec0ec4e1b2c8a84880ebc5b7
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-05 20:05:16 +0300

    fix: add a check for log directory, if it doesn't exist, create it

--- vars.py
+++ vars.py
@@ -1,3 +1,5 @@
+import os
+
 # enter your bot token in format "oauth:..."
 GPT_TMI_TOKEN = ""
 
@@ -12,3 +14,7 @@ BOT_PREFIX = "!"
 
 # enter your potential log directory file path, below is just an example
 log_dir = "C:/TwitchBot/logs/"
+# Check if the log folder exists
+if not os.path.exists(log_dir):
+    # Create the log folder if it doesn't exist
+    os.makedirs(log_dir)
