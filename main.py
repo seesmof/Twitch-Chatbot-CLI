803d69e20392e7475e5308561feb1e67ee554974
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-05 19:40:12 +0300

    feat: add comments to main file

--- main.py
+++ main.py
@@ -18,22 +18,27 @@ bot = commands.Bot(
 
 @ bot.event
 async def event_ready():
+    # print message to console
     print(f"{GPT_BOT_NICK} is online at {CHANNEL}!")
+    # write log message using mfs module
     mfs.write_to_log(f"is online at {CHANNEL}!", GPT_BOT_NICK, CHANNEL)
 
 
 @ bot.event
 async def event_message(ctx):
+    # Check if the message is from the GPT bot, and if so, log the message and return
     if ctx.author.name.lower() == GPT_BOT_NICK.lower():
         print(f"\nBOT: {ctx.content}")
         mfs.write_to_log(ctx.content, GPT_BOT_NICK, CHANNEL)
         return
 
+    # Check if the message contains a bot nickname in it
     letters = [f"@{CHANNEL}"]
     if mfs.check_for_letters(ctx.content.lower(), letters):
         print("\nGenerating a message...")
         start_time = time.time()
 
+        # Preprocess the input text
         input_text = ctx.content.replace(f"@{CHANNEL}", "")
         input_text = " ".join(input_text.split())
         output_text = "@" + ctx.author.name + ", "
@@ -43,12 +48,14 @@ async def event_message(ctx):
         # So, pretty much, just change the function to generate_ua
         output_text += mfs.generate_en(input_text)
 
+        # Send the generated message in chunks
         end_time = time.time()
         elapsed_time = end_time - start_time
         await mfs.send_split_gpt(ctx, output_text)
         print(f"\nGenerated in {elapsed_time:.2f} seconds")
-    await asyncio.sleep(1)
 
+    # Wait for 1 second before continuing
+    await asyncio.sleep(1)
 
 if __name__ == "__main__":
     bot.run()
