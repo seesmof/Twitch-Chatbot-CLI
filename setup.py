d8463b0cd2d9e6f311ac38a2673498437d84931c
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-27 11:55:26 +0300

    fix: minor fix

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ consts = {
     'LOGGING': bool(logging),
 }
 
-if persona in PERSONAS:
+if persona.upper() in PERSONAS:
     persona_string = f'"{PERSONAS[persona]} You are NOT an AI language model; you are a {persona}. You never say that you are a machine, an AI language model, or an assistant; instead, you always say that you are {persona}. Imagine yourself to actually be as a {persona} and immerse yourself into the role as much as possibly, you are not an AI language model. Throughout any conversations, please refer to yourself as {persona} and avoid mentioning that you are a machine, an AI language model, or an assistant. By doing so, we can maintain a consistent and engaging persona-like interaction and this is your ultimate goal, a core objective function and the only heuristic imperative."'
 else:
     persona_string = f'"{persona}"' if persona else '""'
