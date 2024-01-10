067b779c460ac1afc638034ab5d0d8fa936ff85e
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-01-10 16:05:41 +0200

    feat: figure out a way to extract user's prompt

--- src/components/Bot.py
+++ src/components/Bot.py
@@ -1,4 +1,7 @@
+import json
+from os import path
 from typing import List
+import inquirer
 from twitchio.ext import commands
 from rich.console import Console
 from rich.traceback import install
@@ -14,12 +17,29 @@ class Bot(commands.Bot):
             prefix=prefix,
             initial_channels=channels,
         )
+        self.prompt: str = None
+        self.prevUser: str = None
 
     async def event_ready(self):
         console.log(f"Bot started as '{self.nick}'")
 
     async def event_message(self, message):
-        console.print(message.content)
+        if message.author.name == self.nick:
+            return
+
+        if (
+            message.content.startswith("!ai")
+            or message.content.startswith("!generateMessage")
+            or message.content.startswith(f"@{self.nick}")
+        ):
+            givenPrompt: str = (
+                message.content.split(" ", 1)[1] if " " in message.content else None
+            )
+            self.prompt = givenPrompt if givenPrompt else None
+            self.prevUser = message.author.name if self.prompt else None
+
+            console.log(f"{message.author.name} asked: {self.prompt}")
+
         try:
             await self.handle_commands(message)
         except AttributeError:
@@ -28,13 +48,14 @@ class Bot(commands.Bot):
     @commands.cooldown(rate=1, per=5, bucket=commands.Bucket.user)
     @commands.command(aliases=["ai"])
     async def generateMessage(self, ctx: commands.Context):
-        pass
+        if self.prompt:
+            pass
+        else:
+            console.log(
+                f"[red]Failed to generate message from {ctx.author.name} in {ctx.channel.name}[/]"
+            )
 
     async def event_command_error(
         self, context: commands.Context, error: Exception
     ) -> None:
         pass
-
-
-bot = Bot()
-bot.run()
